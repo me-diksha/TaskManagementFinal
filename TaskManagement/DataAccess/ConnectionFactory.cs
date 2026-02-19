2a8285185bc078e37baa1eda9262a5d854aba36a
DT-SN007 <DT-SN007@DT-SN007>
2026-02-19 11:39:24 +0530

    Created DataAccess

---
+++ TaskManagement/DataAccess/ConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagement.DataAccess.Interfaces;
+
+namespace TaskManagement.DataAccess
+{
+    public class ConnectionFactory:IConnectionFactory
+    {
+        private readonly string _connectionstring;
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            _connectionstring = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public NpgsqlConnection CreateConnection()
+        {
+            return new NpgsqlConnection(_connectionstring);
+        }
+    }
+}
