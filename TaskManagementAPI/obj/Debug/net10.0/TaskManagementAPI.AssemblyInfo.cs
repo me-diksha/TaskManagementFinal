7906bee6c92581fee7962476836893711e95a384
DT-SN007 <DT-SN007@DT-SN007>
2026-02-27 10:47:39 +0530

    changes

--- TaskManagementAPI/obj/Debug/net10.0/TaskManagementAPI.AssemblyInfo.cs
+++ TaskManagementAPI/obj/Debug/net10.0/TaskManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e475bcc1b1aa59aa66d98df3a82f465bb7ff12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c24ee976dd078f184cca32b496fedd59f720c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
